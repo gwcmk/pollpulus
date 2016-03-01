5c5543960a9c8304eee3efbfcb34ea1d56934f02
gwcmk <gregwcmkim@gmail.com>
2016-02-29 21:20:22 -0600

    Styled questions, new question, and poll pages

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,17 @@
 var currentAnswerNumber = 3;
 $(document).ready(function() {
-  $('#add-answer').click(function() {
+  $('#add-answer').click(function(event) {
     event.preventDefault();
     $(this).before('<div class="form-group">' +
                       '<label for="answer'+ currentAnswerNumber +'">Answer '+ currentAnswerNumber +':</label>' +
                       '<input name="answer'+ currentAnswerNumber +'" type="text" class="form-control" id="answer'+ currentAnswerNumber +'" placeholder="">' +
                     '</div>');
     currentAnswerNumber++;
+  });
+
+  $('#poll a').click(function(event) {
+    event.preventDefault();
+    $('.active').removeClass('active');
+    $(this).addClass('active');
   });
 });
